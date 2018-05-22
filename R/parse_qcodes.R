cccea291356128ce47217c68e6c9ad083d4c025f
Jenny Draper <learithe@gmail.com>
2018-05-22 14:50:26 -0700

    parser can now handle multiple codes per text block

--- R/parse_qcodes.R
+++ R/parse_qcodes.R
@@ -31,14 +31,37 @@ parse_qcodes <- function(filename, doc_id,...){
   #parse the file a kind of foolish way, results in individual entries of form:
   #text(/Qcode){#qcode
   #where text is the text that was coded, and qcode is the code asigned to the text
-  parsed <- unlist( str_extract_all(filestring,pattern=regex( "(?<=(QCODE\\))).*?(?=(\\}))" )) )
+  
+  #parsed <- unlist( str_extract_all(filestring,pattern=regex( "(?<=(QCODE\\))).*?(?=(\\}))" )) )
+  parsed <- unlist( str_extract_all(filestring,pattern=regex( "(?<=(QCODE\\))).*?(?=(\\{#)).*?(?=(\\}))" )) )
   
   #parse each qcode flagged item and add it to the data frame as a new row
   for(item in parsed){
     splititems <- unlist(strsplit(item, "\\(/QCODE\\)\\{#"))
-    rowtoadd <- data.frame(doc = doc_id, qcode = as.factor(splititems[2]), text = splititems[1])
-    df <- rbind(df,rowtoadd)
+    
+    #handle cases where multiple codes are assigned to one text block
+    if( str_detect(splititems[2], ",[ ]*#") ){
+      splitcodes <- unlist(strsplit(splititems[2], ",[ ]*#"))
+      print(splitcodes)
+      
+      for(code in splitcodes){
+        rowtoadd <- data.frame(doc = doc_id, qcode = as.factor(code), text = splititems[1])
+        df <- rbind(df,rowtoadd)
+      }
+    } 
+    #otherwise just add the one entry to the df
+    else{
+      rowtoadd <- data.frame(doc = doc_id, qcode = as.factor(splititems[2]), text = splititems[1])
+      df <- rbind(df,rowtoadd)
+    }
   }
   
   return(df)
 }
+
+###test case
+#dfret <- parse_qcodes(filename = "inst/Example_Data_Markedup/CoC_Example1_mod_MU.txt", doc_id = "1m")
+#View(dfret)
+
+
+
