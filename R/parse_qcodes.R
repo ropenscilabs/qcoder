6a0bb721038f6fcc012cb4a4621d0254324f2ef8
Elin Waring <elin.waring@gmail.com>
2018-05-24 19:56:27 -0700

    Update parser to use the data  frame.

--- R/parse_qcodes.R
+++ R/parse_qcodes.R
@@ -15,40 +15,44 @@
 
 library(stringr, readr)
 
-parse_qcodes <- function(filename, doc_id,...){
-  
+parse_qcodes <- function(x, ...){
+
   #read in the file
-  filestring <- read_file(filename)
+  #filestring <- readr::read_file(filename)
   #replace newlines
-  filestring <- str_replace_all(filestring, "[\r\n]", "<br>")
-  
+  x$document_text <- stringr::str_replace_all(x$document_text, "[\r\n]", "<br>")
+  #filestring <- stringr::str_replace_all(filestring, "[\r\n]", "<br>")
   #define the empty data frame to fill
-  df <- data.frame(doc=character(), qcode=factor(), text=character(), stringsAsFactors = FALSE)
-  
+
+  df <- data.frame(doc = integer(), qcode = factor(),
+                   text = character(), stringsAsFactors = FALSE)
+
   #parse the file for qcodes; results in individual entries of form:
   #coded_text(/Qcode){#qcode, #qcode2
-  parsed <- unlist( str_extract_all(filestring,pattern=regex( "(?<=(QCODE\\))).*?(?=(\\{#)).*?(?=(\\}))" )) )
-  
+  for (i in 1:nrow(x)) {
+  parsed <- unlist( stringr::str_extract_all(x$document_text[i],pattern=regex( "(?<=(QCODE\\))).*?(?=(\\{#)).*?(?=(\\}))" )) )
+  doc_id <- x$doc_id[i]
   #parse each qcode flagged item and add it to the data frame as a new row
   for(item in parsed){
     splititems <- unlist(strsplit(item, "\\(/QCODE\\)\\{#"))
-    
+
     #handle cases where multiple codes are assigned to one text block
     if( str_detect(splititems[2], ",[ ]*#") ){
       splitcodes <- unlist(strsplit(splititems[2], ",[ ]*#"))
-      
+
       for(code in splitcodes){
         rowtoadd <- data.frame(doc = doc_id, qcode = as.factor(code), text = splititems[1])
         df <- rbind(df,rowtoadd)
       }
-    } 
+    }
     #otherwise just add the one entry to the df
     else{
       rowtoadd <- data.frame(doc = doc_id, qcode = as.factor(splititems[2]), text = splititems[1])
       df <- rbind(df,rowtoadd)
     }
   }
-  
+  }
+
   return(df)
 }
 
