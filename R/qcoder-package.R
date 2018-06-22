7e83ba07a8da43e3adc115b7b57dcdca62389a11
Elin Waring <elin.waring@gmail.com>
2018-06-21 23:51:57 -0400

    Update package documentation to export %>% and related items.

---
+++ R/qcoder-package.R
@@ -0,0 +1,14 @@
+#' Code Qualitative Data
+#' A light weight approach to qualitative coding and analysis
+#'
+#' @name qcoder-package
+#' @aliases qcoder
+#' @docType package
+
+NULL
+
+# Imports -----------------------------------------------------------------
+
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
