05654b99a4bb14bf8fba2ebcc07d3d4aaf550935
Elin Waring <elin.waring@gmail.com>
2018-10-03 20:20:38 -0400

    Fix failing test.

--- tests/testthat/test-selection.R
+++ tests/testthat/test-selection.R
@@ -9,10 +9,10 @@ replacement_text <- add_codes_to_selection(selection=selected_text, codes=codes)
 output <- "(QCODE)This sentence(/QCODE){#harrassment,#new_code}"
 output2 <- "This is a test. (QCODE)This sentence(/QCODE){#harrassment,#new_code} is selected."
 
+test_that("checking add codes",
 
-
-#test_that("checking selection replacement routines",
-
-#expect_equal(add_codes_to_selection(selection = selected_text, codes= codes), output),
-#expect_equal(replace_selection(test_text, selected_text, replacement_text), output2)
-#)
+   expect_equal(add_codes_to_selection(selection = selected_text, codes= codes), output)
+)
+test_that("checking selection replacement",
+          expect_equal(replace_selection(test_text, selected_text, replacement_text), output2)
+)
