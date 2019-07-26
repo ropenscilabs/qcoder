747df84f709267eea174c0b523da4a2d02b7d2b9
Elin Waring <elin.waring@gmail.com>
2019-07-25 23:59:28 -0400

    Test modified.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -110,11 +110,11 @@ test_that("Validating a default project data frame list works", {
 #Make a test for the zip function
 test_that("Testing that zip function works", {
   create_qcoder_project(project_name = "my_qcoder_project", sample = TRUE)
-  zip::zip(zipfile = paste0("QCoderProject", "-", "my_qcoder_project" , "-",
-                            Sys.Date(),".zip"), files = getwd(), recurse = TRUE)
-  expect_equal(file.exists(paste0("QCoderProject-my_qcoder_project-" ,
+ zip::zip(zipfile = paste0("QCoderProject-my_qcoder_project-",
+                            Sys.Date(),".zip"), files = "my_qcoder_project", recurse = TRUE)
+   expect_equal(file.exists(paste0(getwd(), "/QCoderProject-my_qcoder_project-" ,
                                    Sys.Date(),".zip")), TRUE)
-  file.remove(paste0("QCoderProject-my_qcoder_project-" ,
+  file.remove(paste0(getwd(),"/QCoderProject-my_qcoder_project-" ,
                      Sys.Date(),".zip"))
   unlink("./my_qcoder_project", recursive = TRUE)
 }
