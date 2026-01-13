ef9e3baeb5702c79555a1a0c5e2c1e7005fccf44
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-01-13 09:23:15 +0100

    docs: fix package-level manual page

--- R/qcoder-package.R
+++ R/qcoder-package.R
@@ -1,10 +1,5 @@
-#' Code Qualitative Data
-#' A light weight approach to qualitative coding and analysis
-#'
-#' @name qcoder-package
-#' @description Light weight coding
-#' @aliases qcoder
-#' @docType "_PACKAGE
+#' @keywords internal
+"_PACKAGE"
 
 NULL
 
